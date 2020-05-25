1475c33cca2f63c57a665c2ec43cbe7bffb4ad61
Burak Şentürk <burak.senturk@appcent.mobi>
2020-05-25 11:17:16 +0300

    Initial Commit

---
+++ MovieList-ING/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieList-ING
+//
+//  Created by Burak Şentürk on 25.05.2020.
+//  Copyright © 2020 Burak Şentürk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
